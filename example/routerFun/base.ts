0b2ab5f4a940aeb97b842a71dda7fcef5aaa6cc4
文军辉 <wenjunhui1219@163.com>
2019-11-29 09:10:35 +0800

    更新文件

--- example/routerFun/base.ts
+++
@@ -1,6 +0,0 @@
-
-module.exports = function base(router): void {
-    router.get('/base/get', function(req, res): void {
-        res.json(req.query);
-    })
-}
--- example/routerFun/index.ts
+++
@@ -1,6 +0,0 @@
-
-const base = require('./base');
-
-module.exports = function(Router): void {
-    base(Router);
-}
