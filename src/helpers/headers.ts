3da1df94f836d86491030fd7f58aa2981fa0f8af
文军辉 <wenjunhui1219@163.com>
2019-12-02 20:32:31 +0800

    更新headers

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -1,10 +1,26 @@
+import {isObject} from "./util";
 
+
+export function normalizeHeaders(headers: any, paramsName: string) : void{
+    if (!headers) {
+        return;
+    }
+
+    Object.keys(headers).forEach(attr => {
+         
+    });
+}
 
 
 
 export function processHeaders(headers: any, data: any): any {
-    // 这里只是要设置header-content-type的内容。
-    
+    // 这里只是要设置header-content-type的内容。 --- 这里相当于
+    normalizeHeaders(headers, "Content-Type");
 
-
+    if (isObject(data)) {
+        if (headers && headers['Conteny-Type']) {
+            headers['Content-type'] = 'application/json; charset=utf-8';   
+        }
+    }
+    return headers;
 }
