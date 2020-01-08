e71152d89b0f40e0530a44018e18538f630e8a22
georgeganlu <wenjunhui1219@163.com>
2020-01-08 08:01:21 +0800

    更新cancelToken class类的实现

--- src/cancel/CancelToken.ts
+++ src/cancel/CancelToken.ts
@@ -1,23 +1,31 @@
 import { CancelExecutor } from '../types'
 
 export interface resolvePromiseFn {
-  (c: string): void
+  (message?: string): void
 }
 
 export default class CancelToken {
   promise: Promise<string>
 
-  reason?: string
+  reason?: string // 在接口的定义里面就决定了这个reason定义的情况 可传可不传，同样在实例的实现上也要保持一致。
 
   constructor(executor: CancelExecutor) {
-    let resolvePromise: resolvePromiseFn
+    let resolvePromise: resolvePromiseFn // 先声明标记这是一个变量
 
     this.promise = new Promise<string>(resolve => {
-      resolvePromise = resolve
+      resolvePromise = resolve // 和正常的promise不同的是
     })
 
     executor(message => {
+      // 这里要防止用户多次调用执行函数。
+      if (this.reason) {
+        return
+      }
       this.reason = message
+      // 在executor里面传入的函数里面执行 promise的 resolve来触发then的执行。
+      resolvePromise()
     })
   }
 }
+
+// 这里的函数多层指向完成 promise的异步取消功能。
--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,81 +1,96 @@
 import { AxiosRequestConfig, AxiosResponse, AxiosPromise } from '../types'
-import { parseHeaders } from '../helpers/util';
-import  createError from '../helpers/error';
+import { parseHeaders } from '../helpers/util'
+import createError from '../helpers/error'
 
-export default function xhr(config: AxiosRequestConfig):AxiosPromise  {
-    // xhr发送请求的三个步骤。
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  // xhr发送请求的三个步骤。
 
-    return new Promise((succ, fail) => {
-        let { url, data = null, method = 'get', headers = {}, responseType, timeout } = config;
-    
-        let req = new XMLHttpRequest();
+  return new Promise((succ, fail) => {
+    let {
+      url,
+      data = null,
+      method = 'get',
+      headers = {},
+      responseType,
+      timeout,
+      cancelToken
+    } = config
 
-        if (responseType) {
-            req.responseType = responseType;
-        }
-        if (timeout) {
-            req.timeout = timeout;
-        }
+    let req = new XMLHttpRequest()
 
-        req.open(method.toUpperCase(), url!, true) // 发送请求默认使用异步的方式。
+    if (responseType) {
+      req.responseType = responseType
+    }
+    if (timeout) {
+      req.timeout = timeout
+    }
 
-        req.onreadystatechange = function () {
-            if (req.readyState !== 4) {   // readyState会连续进行触发直到为 4为止。 这里先不考虑错误。
-                return
-            }
+    req.open(method.toUpperCase(), url!, true) // 发送请求默认使用异步的方式。
 
-            // 响应的状态码在 200- 300之间。
-            
-            // 之后的话，取出接口定义的数据结构。  data  status statusText  headers config request;
-            const responeseHeaders = req.getAllResponseHeaders();
-            const responseData = responseType === 'text' ? req.responseText : req.response;
-            const resultData: AxiosResponse = {
-                data: responseData,
-                status: req.status,
-                statusText: req.statusText,
-                headers: parseHeaders(responeseHeaders),
-                config,
-                request: req,
-            }
-            // 处理响应的参数.
-            handleResponse(resultData);
+    req.onreadystatechange = function() {
+      if (req.readyState !== 4) {
+        // readyState会连续进行触发直到为 4为止。 这里先不考虑错误。
+        return
+      }
 
-        }
+      // 响应的状态码在 200- 300之间。
 
-        req.onerror = function () {
-            fail(createError('Network Error', config, null, req));
-        }
+      // 之后的话，取出接口定义的数据结构。  data  status statusText  headers config request;
+      const responeseHeaders = req.getAllResponseHeaders()
+      const responseData = responseType === 'text' ? req.responseText : req.response
+      const resultData: AxiosResponse = {
+        data: responseData,
+        status: req.status,
+        statusText: req.statusText,
+        headers: parseHeaders(responeseHeaders),
+        config,
+        request: req
+      }
+      // 处理响应的参数.
+      handleResponse(resultData)
+    }
 
-        req.ontimeout = function () {
-            // fail(new Error("请求超时"));
-            fail(createError(`Timeout of ${timeout}`, config, 'ECONNABORTED', req));
-        }
+    req.onerror = function() {
+      fail(createError('Network Error', config, null, req))
+    }
 
-        // 如果要扑捉错误的话，一般错误分成3种。  1种网络请求本身出错，1种超时出错，1种返回状态码不在 200-300之间的出错  总共出错的类型是3种;
+    req.ontimeout = function() {
+      // fail(new Error("请求超时"));
+      fail(createError(`Timeout of ${timeout}`, config, 'ECONNABORTED', req))
+    }
 
+    // 如果要扑捉错误的话，一般错误分成3种。  1种网络请求本身出错，1种超时出错，1种返回状态码不在 200-300之间的出错  总共出错的类型是3种;
 
-        // 发送内容前
-        Object.keys(headers).forEach(name => {
-            // 这里是设置headers的内容，当data数据为空时，content-type就没有设置的必要了，但是其它headers里面的内容是需要的。
-            if (data === null && name.toLowerCase() === 'content-type') {  // 对这个字段一定是同时满足
-                delete headers[name];  // 只是在data数据为空的情况下，只是删除掉content-type这个选项，所以
-            } else {
-                req.setRequestHeader(name, headers[name]);
-            }
-        });
-        req.send(data);
-
-
-        // 处理handle响应.
-        function handleResponse(response: AxiosResponse) {
-            if (response.status >= 200 && response.status < 300) {
-                succ(response);
-            } else {             
-                let message = response.data.message || `Request failed with status code ${response.status}`;   
-                fail(createError(message, config, null, req, response));
-            }
-        }
+    // 发送内容前
+    Object.keys(headers).forEach(name => {
+      // 这里是设置headers的内容，当data数据为空时，content-type就没有设置的必要了，但是其它headers里面的内容是需要的。
+      if (data === null && name.toLowerCase() === 'content-type') {
+        // 对这个字段一定是同时满足
+        delete headers[name] // 只是在data数据为空的情况下，只是删除掉content-type这个选项，所以
+      } else {
+        req.setRequestHeader(name, headers[name])
+      }
     })
+
+    req.send(data)
+
+    // 在发送请求之后进行取消的操作。
+    if (cancelToken) {
+      // 注册执行函数。
+      cancelToken.promise.then(res => {
+        req.abort() // 取消请求。
+        fail(res)
+      })
+    }
+
+    // 处理handle响应.
+    function handleResponse(response: AxiosResponse) {
+      if (response.status >= 200 && response.status < 300) {
+        succ(response)
+      } else {
+        let message = response.data.message || `Request failed with status code ${response.status}`
+        fail(createError(message, config, null, req, response))
+      }
+    }
+  })
 }
-
-
